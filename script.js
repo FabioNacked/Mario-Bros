d6e9f14dead9626a2f582222f63267a02409a393
Fábio Simão <fb_simao@hotmail.com>
2022-09-06 12:03:53 -0300

    atualização do game

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src = 'images/game-over.png';
+        mario.src = 'images/elementos/game-over.png';
         mario.style.width = '85px'
         mario.style.marginLeft = '25px'
     }
